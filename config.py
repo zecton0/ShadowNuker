7788ccf1257c75126e8152a9ee940e5675bb6a45
ShadowTools <167440405+ShadowJurmik@users.noreply.github.com>
2024-04-18 17:57:53 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,42 @@
+EMBED_CONFIG = {
+    "title": "🥋ShadowNinjasOwned🥋",    # Maine Embed Title here           
+    "description": "🥋ShadowNinjasOwned🥋",   # Main Embed Description here             
+    "color": 0xFF5733,   # Change embed color if you want (red)        
+    "fields": [
+        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places    
+        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},
+        {"name": "🥋ShadowNinjas🥋", "value": "🥋ShadowNinjas🥋", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False      
+    ],
+    "image": "https://i.ibb.co/Mfd4Fg0/imagfes.png",   # Embed Icon url here → https://image.jpg      
+    "footer": "🥋ShadowNinjasOwned🥋",  # Embed Footer here     
+}
+
+SERVER_CONFIG = {
+    "new_name": "🥋ShadowNinjas🥋",  # New Server Name here      
+    "new_icon": "https://i.ibb.co/Mfd4Fg0/imagfes.png",   # New Server Icon url here → https://image.jpg    
+    "new_description": "🥋ShadowNinjasOwned🥋",  # New Server Description here    
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "🥋ShadowNinja🥋",  # Webhook Name here     
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 25,  # Number of channels    
+    'channel_type': 'text',  # text/voice     
+    'channel_name': '🥋ShadowNinjas🥋',  # Channel name      
+    'num_messages': 10,  # Number of message to spam     
+    'message_content': '@everyone ちんぽ野郎、その結果がこれだ、くたばれ。🥋ShadowNinjas🥋ちんぽ野郎、その結果がこれだ、くたばれ。https://i.ibb.co/Mfd4Fg0/imagfes.png, https://discord.gg/PpHwQZP23C' # Spam Message     
+}
+
+NO_BAN_KICK_ID = {
+    000000000000,       # Put Whitelist ID
+    111111111111,       # No Ban & No Kick
+    222222222222,
+}
+
+BOT_PRESENCE = {
+    "type": "playing",  # "playing", "listening", or "watching"
+    "text": "SecuritySystem"  # Your text presence
+}
